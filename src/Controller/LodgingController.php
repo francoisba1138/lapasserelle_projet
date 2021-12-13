68722f34bd383bc14bde7148fced1bd99d33fa17
francoisba1138 <francoisba@gmail.com>
2021-12-13 14:34:44 +0100

    creation du controller

---
+++ src/Controller/LodgingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LodgingController extends AbstractController
+{
+    /**
+     * @Route("/lodging", name="lodging")
+     */
+    public function index(): Response
+    {
+        return $this->render('lodging/index.html.twig', [
+            'controller_name' => 'LodgingController',
+        ]);
+    }
+}
